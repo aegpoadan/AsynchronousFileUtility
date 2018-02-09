d1d39b13666db122b5f478a9f8ecc507db652cc0
aegpoadan <ahayes2425@gmail.com>
2018-02-09 15:59:14 -0500

    Improved parallel functionality and thread handling

--- ExecutorUtil.java
+++ ExecutorUtil.java
@@ -15,9 +15,7 @@ import org.apache.log4j.Logger;
  * A Utility class to execute asynchronous tasks.
  */
 public final class ExecutorUtil {
-	@SuppressWarnings("unused")
 	private static final Logger logger;
-	
 	static {
 		logger = LogManager.getLogger(ExecutorUtil.class);;
 	}
@@ -40,11 +38,59 @@ public final class ExecutorUtil {
 	 * @param threadPool the thread pool to use for function execution
 	 * @param functions an array of functions to execute
 	 */
-	public static final void executeTasks(ScheduledExecutorService threadPool, Runnable... functions) {
+	public static final void executeTasks(ExecutorService threadPool, Runnable... functions) {
 		Arrays.stream(functions).parallel()
 		.forEach((function) -> {
 			threadPool.execute(function);
 		});
 	}
+	
+	public static final Future<?> executeTaskAsync(ExecutorService threadPool, Supplier<?> function) {
+		return threadPool.submit(() -> {
+			return function.get();
+		});
+	}
+	
+	public static final void dispatchTaskAsync(ExecutorService threadPool, Runnable function) {
+		threadPool.execute(function);
+	}
+	
+	public static final Type getType(final Class<?> klass, final int pos) {
+	    // obtain anonymous, if any, class for 'this' instance
+	    final Type superclass = klass.getGenericSuperclass();
 
+	    // test if an anonymous class was employed during the call
+	    if ( !(superclass instanceof Class) ) {
+	        throw new RuntimeException("This instance should belong to an anonymous class");
+	    }
+
+	    // obtain RTTI of all generic parameters
+	    final Type[] types = ((ParameterizedType) superclass).getActualTypeArguments();
+
+	    // test if enough generic parameters were passed
+	    if ( pos < types.length ) {
+	        throw new RuntimeException(String.format(
+	           "Could not find generic parameter %d because only %d parameters were passed",
+	              pos, types.length));
+	    }
+
+	    // return the type descriptor of the requested generic parameter
+	    return types[pos];
+	}
+	
+	public static final <T> void executeTaskAsyncCallback(ExecutorService threadPool, Supplier<T> supplier, Consumer<T> callback) {
+		threadPool.execute(() -> {
+			Thread currentThread = Thread.currentThread();
+			
+			try {
+				T result = threadPool.submit(() -> {
+					return supplier.get();
+				}).get();
+				
+				callback.accept(result);
+			}  catch (Exception e) {
+				logger.error("Failed to execute asynchronous task in ExecutorUtil.executeTaskAsyncCallback(). Current thread: " + currentThread.getName(), e);
+			}
+		});
+	}
 }
--- FileUtility.java
+++ FileUtility.java
@@ -27,21 +27,38 @@ import org.apache.log4j.Logger;
  *
  * Utility class to simplify IO in Java. Asynchronous file operations are modeled after file IO in NodeJS. 
  */
-public final class FileUtil {
+public final class FileUtility {
 	private static final Logger logger;
-	private static final String threadPoolName;
-	private static final ScheduledExecutorService asyncFilePool;
+	private static final ExecutorService asyncFilePool = new ForkJoinPool();
 	
 	static {
-		logger = LogManager.getLogger(FileUtil.class);
-		threadPoolName = "asyncFilePool";
-		asyncFilePool = Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
+		logger = LogManager.getLogger(FileUtility.class);
 	}
 	
 	public static final String readFileSync(File file, Charset encoding) throws IOException {
 	  byte[] encoded = Files.readAllBytes(Paths.get(file.getPath()));
 	  return new String(encoded, encoding);
 	}
+	
+	/**
+	 * Convenience method to save a byte[] asynchronously. Returns an Object[] of length 2.
+	 * Index 0 contains the future result of the async save (number of bytes written)
+	 * Index 1 contains the asynchronous file channel, so that it may be closed 
+	 * @param bytes the bytes to save to a file
+	 * @param path the system path to save the file to
+	 * @return Object[] containing the Future<Integer> result and the file channel
+	 * @throws IOException
+	 */
+	public static final Object[] saveBytesAsync(byte[] bytes, String path) throws IOException {
+		Path file = Paths.get(path);
+		AsynchronousFileChannel asyncFile = AsynchronousFileChannel.open(file, 
+				StandardOpenOption.WRITE,
+				StandardOpenOption.CREATE);
+		
+		Future<Integer> pendingResult = asyncFile.write(ByteBuffer.wrap(bytes), 0);
+		return new Object[]{pendingResult, asyncFile};
+	}
+	
 
 	/**
 	 * Saves the passed byte array asynchronously to the specified path. 
@@ -55,8 +72,7 @@ public final class FileUtil {
 	 * @throws InterruptedException
 	 * @throws ExecutionException
 	 */
-	@SafeVarargs
-	public static final void saveBytesAsync(byte[] bytes, String path, Consumer<Integer>... callbacks) throws IOException, InterruptedException, ExecutionException {
+	public static final void saveBytesAsync(byte[] bytes, String path, Consumer<Integer> callback) throws IOException, InterruptedException, ExecutionException {
 		Path file = Paths.get(path);
 		AsynchronousFileChannel asyncFile = AsynchronousFileChannel.open(file, 
 				StandardOpenOption.WRITE,
@@ -68,9 +84,7 @@ public final class FileUtil {
 			try {
 				Integer result = pendingResult.get();
 				asyncFile.close();
-				Arrays.stream(callbacks).parallel().forEach((callback) -> {
-					callback.accept(result);
-				});
+				callback.accept(result);
 			} catch(Exception e) {
 				logger.error("Failed to save file asyncronously in saveBytesAsync()", e);
 			} finally {
@@ -86,6 +100,52 @@ public final class FileUtil {
 	}
 	
 	/**
+	 * Saves the passed byte arrays asynchronously to the specified paths. 
+	 * Once non-blocking operation completes, a callback is executed.
+	 * Callback function(s) will receive one argument; the result of the write operation.
+	 * Each write/callback operation will be executed in parallel.
+	 * @param pathsToMap A map containing system paths as keys, and a byte[] to callback pair as values.
+	 */
+	public static final void saveBytesAsync(Map<String, Map.Entry<byte[], Consumer<Integer>>> pathsToMap) {
+		ExecutorUtil.dispatchTaskAsync(asyncFilePool, () -> {
+			pathsToMap.entrySet().parallelStream().forEach((entry) -> {
+				String path = entry.getKey();
+				byte[] bytes = entry.getValue().getKey();
+				Consumer<Integer> callback = entry.getValue().getValue();
+				
+				Path file = Paths.get(path);
+				try {
+					AsynchronousFileChannel asyncFile = AsynchronousFileChannel.open(file, 
+							StandardOpenOption.WRITE,
+							StandardOpenOption.CREATE);
+					
+					Future<Integer> pendingResult = asyncFile.write(ByteBuffer.wrap(bytes), 0);
+					
+					ExecutorUtil.dispatchTaskAsync(asyncFilePool, () -> {
+						try {
+							Integer result = pendingResult.get();
+							asyncFile.close();
+							callback.accept(result);
+						} catch(Exception e) {
+							logger.error("Failed to save file asyncronously in saveBytesAsync()", e);
+						} finally {
+							if(asyncFile.isOpen()) {
+								try {
+									asyncFile.close();
+								} catch (IOException e) {
+									logger.error("Failed to close asyncronous file stream in saveBytesAsync()", e);
+								}
+							}
+						}
+					});
+				} catch (Exception e) {
+					logger.error("Failed to write bytes to file at path: " + path, e);
+				}
+			});
+		});
+	}
+	
+	/**
 	 * Reads the file at a specified path asynchronously into a byte array.
 	 * Once non-blocking operation completes, a callback(s) is executed.
 	 * Callback function(s) will receive two arguments; the result of the read operation 
@@ -97,8 +157,7 @@ public final class FileUtil {
 	 * @throws InterruptedException
 	 * @throws ExecutionException
 	 */
-	@SafeVarargs
-	public static final void readBytesAsync(String path, BiConsumer<Integer, byte[]>... callbacks) throws IOException, InterruptedException, ExecutionException {
+	public static final void readBytesAsync(String path, BiConsumer<Integer, byte[]> callback) throws IOException, InterruptedException, ExecutionException {
 		Path file = Paths.get(path);
 		if(Files.notExists(file)) {
 			IOException e = new IOException("No file at path: " + path);
@@ -117,9 +176,7 @@ public final class FileUtil {
 			try {
 				Integer result = pendingResult.get();
 				asyncFile.close();
-				Arrays.stream(callbacks).parallel().forEach((callback) -> {
-					callback.accept(result, buffer.array());
-				});
+				callback.accept(result, buffer.array());
 			} catch(Exception e) {
 				logger.error("Failed to save file asyncronously in saveBytesAsync()", e);
 			} finally {
@@ -133,29 +190,59 @@ public final class FileUtil {
 			}
 		});
 	}
-
-	public static final void close() {
-		asyncFilePool.shutdown();
+	
+	/**
+	 * Reads the files at specified paths asynchronously into byte arrays.
+	 * Once non-blocking operation completes, a callback is executed.
+	 * Callback function(s) will receive two arguments; the result of the read operation 
+	 * and the read byte array.
+	 * Each file read and callback is executed in parallel.
+	 * @param pathsToCallbacks A map containing system paths as keys, and callbacks as values
+	 */
+	public static final void readBytesAsync(Map<String, BiConsumer<Integer, byte[]>> pathsToCallbacks) {
+		ExecutorUtil.dispatchTaskAsync(asyncFilePool, () -> {
+			pathsToCallbacks.entrySet().parallelStream().forEach((entry) -> {
+				String path = entry.getKey();
+				BiConsumer<Integer, byte[]> callback = entry.getValue();
+				
+				Path file = Paths.get(path);
+				if(Files.notExists(file)) {
+					IOException e = new IOException("No file at path: " + path);
+					logger.error("Attempted to read non-existent file in readBytesAsync()", e);
+				}
+				long numBytes = file.toFile().length();
+				
+				try { 
+					AsynchronousFileChannel asyncFile = AsynchronousFileChannel.open(file, 
+							StandardOpenOption.READ);
+					ByteBuffer buffer = ByteBuffer.allocate(toIntExact(numBytes));
+					Future<Integer> pendingResult = asyncFile.read(buffer, 0);
+					
+					ExecutorUtil.dispatchTaskAsync(asyncFilePool, () -> {
+						try {
+							Integer result = pendingResult.get();
+							asyncFile.close();
+							callback.accept(result, buffer.array());
+						} catch(Exception e) {
+							logger.error("Failed to save file asyncronously in saveBytesAsync()", e);
+						} finally {
+							if(asyncFile.isOpen()) {
+								try {
+									asyncFile.close();
+								} catch (IOException e) {
+									logger.fatal("Failed to close asyncronous file stream in saveBytesAsync()", e);
+								}
+							}
+						}
+					});
+				} catch(Exception e) {
+					logger.error("Failed to read file: " + path + "\nIn FileUtil.readBytesAsync()", e);
+				}
+			});
+		});
 	}
 	
-	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
-		String test = "Hello world!";
-		byte[] bytesToSave = test.getBytes();
-		
-		saveBytesAsync(bytesToSave, "/Users/hayesap/Downloads/test.txt", (result) -> {
-			if(result == bytesToSave.length) {
-				System.out.println("File was succesfully saved!");
-				
-				try {
-					readBytesAsync("/Users/hayesap/Downloads/test.txt", (readResult, bytes) -> {
-						System.out.println(new String(bytes));
-						close();
-					});
-				} catch (Exception e) {
-					logger.error("Failed to read file", e);
-					close();
-				} 
-			}
-		});
+	public static final void close() {
+		asyncFilePool.shutdown();
 	}
 }
